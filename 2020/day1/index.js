76eb44d358fc2307548c9b1e4ec169387a233deb
GHSoares <ghsoares99795@gmail.com>
2021-12-07 15:26:55 -0300

    Added 2020/day1

---
+++ 2020/day1/index.js
@@ -0,0 +1,27 @@
+// https://adventofcode.com/2020/day/1
+import { loadInputLines } from '../../utils.js';
+const numbers = loadInputLines().map(l => Number(l));
+const numNumbers = numbers.length;
+
+let [num1, num2, num3] = [null, null, null];
+let found = false;
+
+for (let i = 0; i < numNumbers; i++) {
+	for (let j = 0; j < numNumbers; j++) {
+		for (let k = 0; k < numNumbers; k++) {
+			if (i === j || i === k || k === j) continue;
+	
+			const sum = numbers[i] + numbers[j] + numbers[k];
+			
+			if (sum === 2020) {
+				[num1, num2, num3] = [numbers[i], numbers[j], numbers[k]];
+				found = true;
+				break;
+			}
+		}
+		if (found) break;
+	}
+	if (found) break;
+}
+
+console.log(num1 * num2 * num3);
--- index.js
+++ index.js
@@ -1,14 +1,76 @@
+import yargs from 'yargs';
+import { hideBin } from 'yargs/helpers';
+import fs from 'fs';
+import path from 'path';
 import { exec } from 'child_process';
 
-const year = '2021';
-const day = 'day1';
+const createChallengeFolder = (year, day) => {
+	const folder = `${year}/day${day}`;
 
-const cmd = `cd ./${year}/${day} && node index.js`;
-console.log(`executing ${cmd}`);
-exec(cmd, (error, stdout, stderr) => {
-	process.stdout.write(stdout);
-	process.stderr.write(stderr);
-	if (error !== null) {
-		console.log(`exec error: ${error}`);
-	}
-});
+	const index = `// https://adventofcode.com/${year}/day/${day}\nimport { loadInputLines } from '../../utils.js';`;
+
+	if (!fs.existsSync(folder)) fs.mkdirSync(folder);
+
+	fs.writeFileSync(path.join(folder, `index.js`), index);
+	fs.writeFileSync(path.join(folder, `input.txt`), '');
+}
+
+const runChallenge = (year, day) => {
+	const cmd = `cd ./${year}/day${day} && node index.js`;
+
+	console.log(`executing ${cmd}`);
+	exec(cmd, (error, stdout, stderr) => {
+		process.stdout.write(stdout);
+		process.stderr.write(stderr);
+		if (error !== null) {
+			console.log(`exec error: ${error}`);
+		}
+	});
+}
+
+yargs(hideBin(process.argv))
+	.command('create', 'Creates the challenge folder', {
+		year: {
+			describe: 'The challenge year, defaults to the current year',
+			default: -1,
+			alias: 'y',
+			type: 'number'
+		},
+		day: {
+			describe: 'The challenge day, defaults to the current day',
+			default: -1,
+			alias: 'd',
+			type: 'number'
+		},
+
+	}, argv => {
+		let { day, year } = argv;
+		if (year === -1) year = new Date(Date.now()).getFullYear();
+		if (day === -1) day = new Date(Date.now()).getDate();
+
+		createChallengeFolder(year, day);
+	})
+
+	.command('run', 'Runs a challenge', {
+		year: {
+			describe: 'The challenge year, defaults to the current year',
+			default: -1,
+			alias: 'y',
+			type: 'number'
+		},
+		day: {
+			describe: 'The challenge day, defaults to the current day',
+			default: -1,
+			alias: 'd',
+			type: 'number'
+		},
+
+	}, argv => {
+		let { day, year } = argv;
+		if (year === -1) year = new Date(Date.now()).getFullYear();
+		if (day === -1) day = new Date(Date.now()).getDate();
+
+		runChallenge(year, day);
+	})
+
+	.parse();
